6f74a3c1fef52c7c301af68c873ccb093a888943
Alex Han <ahan@1x-vl908-128-119-224-249.wireless.umass.edu>
2018-10-13 22:21:14 -0400

    Moved files around

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,15 +13,35 @@ import Game from './Components/CoinComponent';
 import Table from './Components/HistoryComponent';
 import Page from './Components/PageComponent';
 
+// server.js
+
+const User    = require('./app/models/user.js')
+var   express = require('express');
+var   app     = express();
+const http    = require('http')
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const server = http.createServer(app)
+app.post('/signup', User.signup)
+// app.post('/signin', User.signin)
+
+const port = 3000;
+server.listen(port, () => {
+  console.log("Server started on port " + port);
+})
+
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-    <Route exact path="/" component={Page} />
-    <Route path="/coin" component={Game} />
-    <Route path="/table" component={Table} />
+      <Route exact path="/" component={Page} />
+      <Route path="/coin" component={Game} />
+      <Route path="/table" component={Table} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
+
 registerServiceWorker();
